a5ca2b819c4ca74c63af51cc8ed9e7aaeda82ba3
acexsre <69ayushsharma@gmail.com>
2023-12-31 01:13:21 +0530

    Basic Structure created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './component/customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './component/customer-details/customer-details.component';
+import {MaterialUiModule} from './materialui.module'
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialUiModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/model/Customer.model.ts
@@ -0,0 +1,9 @@
+export interface Customers {
+    id: number,
+    name: string,
+    email: string,
+    address: string,
+    totalpoints: number,
+    level: string,
+    status: string
+}
